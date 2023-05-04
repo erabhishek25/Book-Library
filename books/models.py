811a723b5dda73ec0b64521bef5b99bd53a97ebd
erabhisek <abhishek.er45@gmail.com>
2023-05-04 22:47:18 +0530

    Added new app book with book listing functionality

---
+++ books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+
+	title = models.CharField(max_length=255)
+	author = models.CharField(max_length=255)
+	genre = models.CharField(max_length=100)
+	height = models.CharField(max_length=10)
+	publisher = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.title
+
