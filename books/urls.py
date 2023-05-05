73eaceedb80f5f4c7a51e9e2bc50dfa0e6abd1ba
erabhisek <abhishek.er45@gmail.com>
2023-05-05 11:09:56 +0530

    Create new app api and do book functionality

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BookListView
+from .views import BookListView, upload_csv
 
 
 urlpatterns = [
 
-	path('', BookListView.as_view(), name='book-list')
+	path('', BookListView.as_view(), name='book_list'),
+	path('csv/', upload_csv, name='upload_csv'),
 ]
